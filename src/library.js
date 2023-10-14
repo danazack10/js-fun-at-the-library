1f3ecc64867b9c4682143e7d5bd441b96f8f7ba5
Dana Zack <danaezack@gmail.com>
2023-10-14 16:01:30 -0600

    Complete first two functions

--- src/library.js
+++ src/library.js
@@ -1,8 +1,40 @@
+function createLibrary(Libraryname){
+  var library = {
+    name: Libraryname,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: [],
+    }
+    }
+    return library
+  };
 
+// function addBook(library, bookName){
+//   return library.shelves.fantasy.push(bookName);
+// }
+
+function addBook(library, bookName){
+  if (bookName.genre === "fantasy"){
+    return library.shelves.fantasy.push(bookName);
+  } else if (bookName.genre === "fiction"){
+    return library.shelves.fiction.push(bookName);
+  } else {
+    return library.shelves.nonFiction.push(bookName);
+  }
+}
+
+function checkoutBook(){
+
+}
+
+function takeStock(){
+
+}
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook,
-  // takeStock
+  createLibrary,
+  addBook,
+  checkoutBook,
+  takeStock
 };
