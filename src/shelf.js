5ad13326d912b89ba55fe0d0e98578742ef5a33b
Dana Zack <danaezack@gmail.com>
2023-10-15 06:40:48 -0600

    Complete first function shelfBook

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,52 @@
+// function shelfBook(book, typeOfShelf){
+//   var updatedShelf = typeOfShelf.push(book);
+//   return updatedShelf;
+// }
+
+// function shelfBook(book, typeOfShelf){
+//   if (book.genre === "sciFi"){
+//     typeOfShelf[typeOfShelf.length] = book;
+//     return typeOfShelf
+//   } else {
+//     return typeOfShelf
+//   }
+// }
+
+// Non-functioning
+// =====================================================
+// Functioning
+
+// function shelfBook(book, typeOfShelf){
+//   return typeOfShelf.unshift(book);
+// }
+
+// function shelfBook(book, typeOfShelf){
+//   if (book.genre === "sciFi"){
+//     var updatedShelf = typeOfShelf.unshift(book);
+//     return updatedShelf;
+//   } else {
+//     return typeOfShelf;
+//   }
+// }
+
+function shelfBook(book, typeOfShelf){
+  if (book.genre === "sciFi" && typeOfShelf.length < 3){
+    return typeOfShelf.unshift(book);
+  } else {
+    return typeOfShelf;
+  }
+}
+
+// ===================================================
+
+function unshelfBook(){
+
+}
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
+  shelfBook,
+  unshelfBook,
   // listTitles,
   // searchShelf
 };
+
