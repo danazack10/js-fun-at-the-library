bb43e30aded06d77516d4ed7df1a7520d1e1f96d
Dana Zack <danaezack@gmail.com>
2023-10-15 06:41:22 -0600

    Complete first function shelfBook

--- test/shelf-test.js
+++ test/shelf-test.js
@@ -9,11 +9,11 @@ var {
 
 describe("shelf.js", function() {
   describe("shelfBook", function() {
-    it.skip("should be a function", function() {
+    it("should be a function", function() {
       assert.isFunction(shelfBook);
     });
 
-    it.skip("should add books to a specific shelf", function() {
+    it("should add books to a specific shelf", function() {
       var hyperion = {
         title: "Hyperion",
         mainCharacter: { name: "The Shrike", age: null, pronouns: "they" },
@@ -30,12 +30,13 @@ describe("shelf.js", function() {
 
       shelfBook(hyperion, sciFiShelf);
       shelfBook(dune, sciFiShelf);
+      // parameters are an object and an array
 
       assert.equal(sciFiShelf[0], dune);
       assert.equal(sciFiShelf[1], hyperion);
     });
 
-    it.skip("should add another book to the shelf", function() {
+    it("should add another book to the shelf", function() {
       var hyperion = {
         title: "Hyperion",
         mainCharacter: { name: "The Shrike", age: null, pronouns: "they" },
@@ -64,7 +65,7 @@ describe("shelf.js", function() {
       assert.equal(sciFiShelf[2], hyperion);
     });
 
-    it.skip("shelves can only hold a certain amount of books", function () {
+    it("shelves can only hold a certain amount of books", function () {
       var hyperion = {
         title: "Hyperion",
         mainCharacter: { name: "The Shrike", age: null, pronouns: "they" },
