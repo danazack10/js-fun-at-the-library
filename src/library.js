9552a11545dbfcd00c34c3bb14245ce47dafc9d0
Dana Zack <danaezack@gmail.com>
2023-10-17 08:42:23 -0600

    Update checkoutBook function

--- src/library.js
+++ src/library.js
@@ -10,12 +10,7 @@ function createLibrary(Libraryname){
     return library
   };
 
-
 // =========================================================
-
-// function addBook(library, bookName){
-//   return library.shelves.fantasy.push(bookName);
-// }
 
 function addBook(library, bookName){
   if (bookName.genre === "fantasy"){
@@ -29,18 +24,38 @@ function addBook(library, bookName){
 
 // =========================================================
 
-// Notes: library is an array of objects, and bookName & 
-// genre are both key values within objects within the library array
-
 // PSEUDOCODE
-// 1) see if genre within shelf contains bookName
+// this will be a function w/ 3 parameters
+// - P1 (library = object) = library.shelves contain 3 genre properties holding arrays of book objects
+// - P2 (bookName = string) = accessed via library.shelves.insertgenre[0].title
+// - P3 (genre = string) = accessed via library.shelves.insertgenre[0].genre
+// return/output will be an interpolated string
+// 
+// STEPS
+// 1. Identify if book is present on the shelf
+// 2. Remove book from shelf
+// 3. Print out string statement that it has been removed 
 
 function checkoutBook(library, bookName, genre){
-  if (Object.keys(library.shelves).includes(genre) === true) {
-    library.shelves.genre.splice[0, 1]
-    return `You have now checked out ${bookName} from ${library}`
-  }
+  for (var i = 0; i < library.shelves.fantasy.length; i++)
+    if (Object.values(library.shelves.fantasy[i]).includes(genre) === true) {
+      library.shelves.fantasy.splice(i, 0)
+    } else {
+    }
+  for (var i = 0; i < library.shelves.fiction.length; i++)
+    if (Object.values(library.shelves.fiction[i]).includes(genre) === true) {
+      library.shelves.fiction.splice(i, 0)
+    } else {
+    }
+  for (var i = 0; i < library.shelves.nonFiction.length; i++)
+    if (Object.values(library.shelves.nonFiction[i]).includes(genre) === true) {
+    library.shelves.nonFiction.splice(i, 0)
+    } else {
+    }
+  return `You have now checked out ${bookName} from ${library}`
 }
+
+
 
 // function checkoutBook(library, bookName, genre){
 //   for (var i = 0; i < library.shelves.fiction.length; i++){
