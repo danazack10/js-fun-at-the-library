3420da9492c1717a524d71b5d99a668ac8a003bc
Dana Zack <danaezack@gmail.com>
2023-10-17 16:20:26 -0600

    Complete checkoutBook function

--- src/library.js
+++ src/library.js
@@ -24,56 +24,43 @@ function addBook(library, bookName){
 
 // =========================================================
 
-// PSEUDOCODE
-// this will be a function w/ 3 parameters
-// - P1 (library = object) = library.shelves contain 3 genre properties holding arrays of book objects
-// - P2 (bookName = string) = accessed via library.shelves.insertgenre[0].title
-// - P3 (genre = string) = accessed via library.shelves.insertgenre[0].genre
-// return/output will be an interpolated string
-// 
-// STEPS
-// 1. Identify if book is present on the shelf
-// 2. Remove book from shelf
-// 3. Print out string statement that it has been removed 
-
 function checkoutBook(library, bookName, genre){
-  for (var i = 0; i < library.shelves.fantasy.length; i++)
-    if (Object.values(library.shelves.fantasy[i]).includes(genre) === true) {
-      library.shelves.fantasy.splice(i, 0)
-    } else {
+  if (genre === "fantasy"){
+    for (var i = 0; i < library.shelves.fantasy.length; i++){
+      if (Object.values(library.shelves.fantasy[i]).includes(bookName) === true) {
+        library.shelves.fantasy.splice(i, 1)
+        return `You have now checked out ${bookName} from the ${library.name}.`
+      }
     }
-  for (var i = 0; i < library.shelves.fiction.length; i++)
-    if (Object.values(library.shelves.fiction[i]).includes(genre) === true) {
-      library.shelves.fiction.splice(i, 0)
-    } else {
+    return `Sorry, there are currently no copies of ${bookName} available at the ${library.name}.`
+  } else if (genre === "fiction"){
+    for (var i = 0; i < library.shelves.fiction.length; i++){
+      if (Object.values(library.shelves.fiction[i]).includes(bookName) === true) {
+        library.shelves.fiction.splice(i, 1)
+        return `You have now checked out ${bookName} from the ${library.name}.`
+      }
     }
-  for (var i = 0; i < library.shelves.nonFiction.length; i++)
-    if (Object.values(library.shelves.nonFiction[i]).includes(genre) === true) {
-    library.shelves.nonFiction.splice(i, 0)
-    } else {
+    return `Sorry, there are currently no copies of ${bookName} available at the ${library.name}.`
+  } else if (genre === "nonFiction"){
+    for (var i = 0; i < library.shelves.nonFiction.length; i++){
+      if (Object.values(library.shelves.nonFiction[i]).includes(bookName) === true) {
+        library.shelves.nonFiction.splice(i, 1)
+        return `You have now checked out ${bookName} from the ${library.name}.`
+      }
     }
-  return `You have now checked out ${bookName} from ${library}`
+    return `Sorry, there are currently no copies of ${bookName} available at the ${library.name}.`
+  }
 }
-
-
-
-// function checkoutBook(library, bookName, genre){
-//   for (var i = 0; i < library.shelves.fiction.length; i++){
-//     if (genre === )
-//     if (bookName.genre === "fantasy"){
-//       return library.shelves.fantasy.push(bookName);
-//     } else if (bookName.genre === "fiction"){
-//       return library.shelves.fiction.push(bookName);
-//     } else {
-//       return library.shelves.nonFiction.push(bookName);
-//     }
-//   }
-// }
 
 // =========================================================
 
-function takeStock(){
-
+function takeStock(library, genre){
+  var shelfStock = [];
+  for (var i = 0; i < library.shelves.fantasy.length; i++)
+    if (Object.values(library.shelves.fantasy[i]).includes(genre)) {
+    // need to then calculate length of array and print it to console
+    }
+    // return length of array in interpolated string
 }
 
 module.exports = {
